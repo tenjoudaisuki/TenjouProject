f504be9d4bbf25fd00d5ce2847c169442aa9676d
unknown <>
2016-10-23 18:58:41 +0900

    バックログ更新 移動方向にプレイヤーのモデルを向けるように変更 移動アニメーション追加 転がりに着手（ＬＳＨＩＦＴ押してる間発動、完了には程遠い）

--- GRAVYRINTH/Assets/Moriya/Script/Cam.cs
+++ GRAVYRINTH/Assets/Moriya/Script/Cam.cs
@@ -3,100 +3,64 @@ using System.Collections;
 
 public class Cam : MonoBehaviour
 {
-    /*==所持コンポーネント==*/
-    private Transform tr;
+    [SerializeField, TooltipAttribute("注視点の対象")]
+    public Transform Target;
+    [SerializeField, TooltipAttribute("注視点との距離")]
+    public float Distance;
+    [SerializeField, TooltipAttribute("X回転の上限下限")]
+    public float XAngleLimit;
+    [SerializeField, TooltipAttribute("注視点の調整")]
+    public Vector3 TargetOffset;
 
-    /*==外部設定変数==*/
-    [SerializeField, TooltipAttribute("カメラを向けるターゲット")]
-    private Transform m_Target;
-    [SerializeField, TooltipAttribute("肩越し視点のときのカメラの位置")]
-    private Transform m_OverShoulder;
-    [SerializeField, TooltipAttribute("カメラの注視点の高さ（ターゲットからどれだけ高い座標を視るか）")]
-    private float m_TargetHeight = 0.0f;
-    [SerializeField, TooltipAttribute("ターゲットからの距離")]
-    private float m_TargetMaxDistance = 1.5f;
-    [SerializeField, TooltipAttribute("アングルが回る速さ係数(0.0～1.0)")]
-    private float m_AngleSpeed = 0.2f;
-    [SerializeField, TooltipAttribute("注視点が移行する速さ（通常←→肩越し　のときのカメラの切り替わり）")]
-    private float m_LookPosChangeSpeed = 0.2f;
-    [SerializeField, TooltipAttribute("注視点が移行し始めるまでにかかる長押しの長さ（秒）")]
-    private float m_LookPosChangeTime = 0.2f;
-    [SerializeField, TooltipAttribute("ターゲットの高さ")]
-    private float m_Height = 1.0f;
+    private Vector3 nextPoint;
+    private float XRotationTotal = 0;
 
-    /*==内部設定変数==*/
-    //線形補間用
-    private Vector3 m_Pos;
-    private Vector3 m_StartPos;
-    private Vector3 m_EndPos;
-    //カメラ注視点
-    private Vector3 m_LookPos;
-    private Vector3 m_StartLookPos;
-    private Vector3 m_EndLookPos;
-
-    Quaternion front;
-
-    //クリックしている時間を計測するタイマー
-    private float m_LeftClickTimer;
-
-
-    /*==================*/
-    /* 生成前初期化   */
-    /*==================*/
-    void Awake()
-    {
-        //コンポーネント取得
-        tr = GetComponent<Transform>();
-    }
-
+    // Use this for initialization
     void Start()
     {
-        front = m_Target.rotation;
+        nextPoint = new Vector3(0, 0, -1);
+        //transform.position = Target.position * Offset;
     }
 
-    /*==================*/
-    /* 更新後更新処理   */
-    /*==================*/
-    void LateUpdate()
+    public void LateUpdate()
     {
-        //クリックタイマー変化
-        if (Input.GetMouseButton(0))
-            m_LeftClickTimer += Time.deltaTime;
-        else if (Input.GetMouseButtonUp(0))
-            m_LeftClickTimer = 0.0f;
 
-        if (m_LeftClickTimer < m_LookPosChangeTime)
+
+        NextPointMove();
+
+        Ray ray = new Ray(Target.position + TargetOffset, nextPoint.normalized);
+
+        Debug.DrawRay(Target.position + TargetOffset, nextPoint);
+
+        CameraMove((Target.position + TargetOffset) + nextPoint * Distance);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10))
         {
-            //通常時
-            float X = Input.GetAxis("Horizontal2") * 2.0f;
-            //angleY = -Input.GetAxis("Vertical2");
-            front *= Quaternion.AngleAxis(X, m_Target.up);
-
-            //移動前の座標
-            m_StartPos = tr.position;
-            //最終的な移動目標
-            m_EndPos = m_Target.position + m_Target.up * m_Height - (front * Vector3.forward) * m_TargetMaxDistance;
-            //移動後の座標
-            m_Pos = Vector3.Lerp(m_StartPos, m_EndPos, m_AngleSpeed);
-
-
-            //注視点
-            m_StartLookPos = m_LookPos;
-            m_EndLookPos = new Vector3(m_Target.position.x, m_Target.position.y + m_Height + m_TargetHeight, m_Target.position.z);
-            m_LookPos = Vector3.Lerp(m_StartLookPos, m_EndLookPos, m_LookPosChangeSpeed);
-
-            //地面にヒットしているか
-            Ray ray = new Ray(m_LookPos, Vector3.Normalize(m_Pos - m_LookPos));
-            RaycastHit hit;
-            bool ishit = Physics.Raycast(ray, out hit, m_TargetMaxDistance);
-            if (ishit)
-            {
-                //ヒットしていた場合、ヒットした地点にカメラを移動
-                m_Pos = hit.point;
-            }
-
-            tr.position = m_Pos;
-            tr.LookAt(m_LookPos);
+            CameraMove(hit.point);
         }
+
+        transform.up = Target.up;
+        transform.LookAt(Target.position + TargetOffset);
+    }
+
+    //カメラが次に行きたい位置を変更
+    private void NextPointMove()
+    {
+        float X = -Input.GetAxis("Horizontal2");
+        float Y = -Input.GetAxis("Vertical2");
+
+        nextPoint = Quaternion.Euler(0, X, 0) * nextPoint;
+
+        XRotationTotal += Y;
+        XRotationTotal = Mathf.Clamp(XRotationTotal, -XAngleLimit, XAngleLimit);
+        if (XRotationTotal < XAngleLimit && -XAngleLimit < XRotationTotal)
+        {
+            nextPoint = Quaternion.AngleAxis(Y, transform.right) * nextPoint;
+        }
+    }
+
+    private void CameraMove(Vector3 next)
+    {
+        transform.position = Vector3.Lerp(transform.position, next, 0.8f);
     }
 }
--- GRAVYRINTH/Assets/Moriya/Script/PlayerMove.cs
+++ GRAVYRINTH/Assets/Moriya/Script/PlayerMove.cs
@@ -2,6 +2,7 @@
 /**
  * プレイヤーの移動（通常時）
  * 歩く、斜面を歩く、段差を登る。
+ * アニメーションも行う。
  * 重力の方向のセットもここで行う。
  * 作成者：守屋   作成日：16/10/14
 /**==========================================================================*/
@@ -11,23 +12,43 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour 
 {
+    //当たったかどうか付のRaycastHit
+    struct RayHitInfo
+    {
+        public RaycastHit hit;
+        //当たったか？
+        public bool isHit;
+    };
+
     /*==所持コンポーネント==*/
     private Transform tr;
-    private GravityDirection m_GravityDirection;
+
     private Animator m_Animator;
+
 
     /*==外部設定変数==*/
     [SerializeField, TooltipAttribute("移動速度")]
     private float m_MoveSpeed = 3.0f;
-    [SerializeField, TooltipAttribute("身長(Rayで使用)")]
+    [SerializeField, TooltipAttribute("身長(地面との判定を行うRayで使用)")]
     private float m_Height = 2.0f;
+    [SerializeField, TooltipAttribute("斜面と認識する角度（壁と斜面の境界値）")]
+    private float m_SlopeDeg = 45.0f;
 
     /*==内部設定変数==*/
+    //重力の方向を所持するクラス。プレイヤー以外で重力を扱う場合こちらのクラスを使用してください。
+    private GravityDirection m_GravityDir;
+    //プレイヤーモデルのトランスフォーム（子オブジェクト）
+    private Transform m_ModelTr;
+    //プレイヤーモデルのＹ軸回転量
+    private float m_ModelRotateY;
 
+    //カメラ
+    private Transform m_Camera;
 
+    //移動方向
+    private Vector3 m_MoveVec;
 
     /*==外部参照変数==*/
-
 
     void Awake()
     {
@@ -36,19 +57,24 @@ public class PlayerMove : MonoBehaviour
         m_Animator = GetComponent<Animator>();
     }
 
-	void Start ()
+	void Start()
     {
-        //コンポーネント取得
-        m_GravityDirection = GameObject.Find("GravityDirection").GetComponent<GravityDirection>();
+        //オブジェクト取得
+        m_GravityDir = GameObject.Find("GravityDirection").GetComponent<GravityDirection>();
+        m_ModelTr = tr.FindChild("Model");
+        m_Camera = Camera.main.transform;;
+
+        //値初期化
+        m_ModelRotateY = 0.0f;
 	}
 	
-	void Update () 
+	void Update() 
     {
         //移動処理
         Move();
 
         //重力の方向を設定
-        m_GravityDirection.SetDirection(GetDown());
+        m_GravityDir.SetDirection(GetDown());
 	}
 
     /// <summary>
@@ -103,14 +129,90 @@ public class PlayerMove : MonoBehaviour
     /// </summary>
     /// <param name="reyPos">レイを飛ばす位置</param>
     /// <returns></returns>
-    private RaycastHit CheckGroundHit(Vector3 reyPos)
+    private RayHitInfo CheckGroundHit(Vector3 reyPos)
     {
+        RayHitInfo result;
         Ray ray = new Ray(reyPos, GetDown());
         RaycastHit hit;
-        Physics.Raycast(ray, out hit, m_Height);
-        Debug.DrawRay(reyPos, GetDown() * m_Height, Color.grey, 1.0f, false);
+        result.isHit = Physics.Raycast(ray, out hit, m_Height);
+        result.hit = hit;
+        
+        //レイをデバッグ表示
+        Debug.DrawRay(reyPos, GetDown() * m_Height, Color.grey, 1.0f, false);       
 
-        return hit;
+        return result;
+    }
+
+    /// <summary>
+    /// 通常移動処理
+    /// </summary>
+    private void NormalMove()
+    {
+        //移動方向入力
+        Vector2 inputVec = GetMoveInputAxis();
+
+        //アニメーション
+        m_Animator.SetBool("InputMove", inputVec.magnitude > 0.0f);
+        //入力方向からモデルの回転量を求める（あまりイケてないのでいい方法あったら教えて）
+        if (inputVec.magnitude > 0.1f)
+        {
+            //入力方向を角度に変換
+            m_ModelRotateY = Mathf.Atan2(-inputVec.y, inputVec.x) * Mathf.Rad2Deg;
+            //カメラの向きとプレイヤーのデフォルト向きを加味して補正
+            m_ModelRotateY += m_Camera.localEulerAngles.y + 90.0f;
+            //回転
+            Vector3 angles = m_ModelTr.localEulerAngles;
+            angles.y = m_ModelRotateY;
+            m_ModelTr.localEulerAngles = angles;
+        }
+
+        //入力された値を移動用に補正
+        Vector2 moveVec = MoveInputCorrection(inputVec);
+        //進行方向である右方向と前方向を決定
+        Vector3 right = m_Camera.transform.right;
+        Vector3 front = -Vector3.Cross(tr.up, right);
+        //移動方向を計算
+        m_MoveVec = (front * moveVec.y + right * moveVec.x) * m_MoveSpeed;
+        //移動
+        tr.position += m_MoveVec * Time.deltaTime;
+
+        //仮重力
+        tr.position += GetDown() * 1.0f * Time.deltaTime;
+        //地面との判定
+        RayHitInfo hitInfo = CheckGroundHit(tr.position + tr.up * m_Height);
+        if (hitInfo.isHit)
+        {
+            //上方向と平面の法線方向のなす角
+            float angle = Vector3.Angle(tr.up, hitInfo.hit.normal);
+            //斜面として認識する角度以上なら何もしない
+            if (angle > m_SlopeDeg) return;
+
+            //当たった地点に移動
+            tr.position = hitInfo.hit.point;
+            //上方向を当たった平面の法線方向に変更
+            tr.up = hitInfo.hit.normal;
+        }
+    }
+
+    /// <summary>
+    /// 転がり移動処理
+    /// </summary>
+    private void RollMove()
+    {
+        //移動
+        tr.position += m_MoveVec * Time.deltaTime;
+        //減速
+        m_MoveVec *= 0.99f;
+
+        //仮重力
+        tr.position += GetDown() * 2.0f * Time.deltaTime;
+        //地面との判定
+        RayHitInfo hitInfo = CheckGroundHit(tr.position + tr.up * m_Height);
+        if (hitInfo.isHit)
+        {
+            //当たった地点に移動
+            tr.position = hitInfo.hit.point;
+        }
     }
 
     /// <summary>
@@ -118,28 +220,19 @@ public class PlayerMove : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        //移動
-        Vector2 inputVec = MoveInputCorrection(GetMoveInputAxis());
-
-        //カメラのトランスフォーム
-        Transform cam = Camera.main.transform;
-        //進行方向
-        Vector3 right = cam.transform.right;
-        Vector3 front = -Vector3.Cross(tr.up, right);
-        //移動
-        tr.position += (front * inputVec.y + right * inputVec.x) * m_MoveSpeed * Time.deltaTime;
-
-        //仮重力
-        tr.position += GetDown() * 1.0f * Time.deltaTime;
-
-        //地面との判定
-        RaycastHit hit = CheckGroundHit(tr.position + tr.up * m_Height);
-        if (hit.normal != Vector3.zero)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            tr.position = hit.point;
-            tr.up = hit.normal;
+            m_Animator.SetBool("InputRoll", true);
+            m_Animator.SetBool("InputMove", false);
+            RollMove();
+        }
+        else
+        {
+            m_Animator.SetBool("InputRoll", false);
+            NormalMove();
         }
     }
+
 
     /// <summary>
     /// ジャンプ処理（小杉さんのタスク）
