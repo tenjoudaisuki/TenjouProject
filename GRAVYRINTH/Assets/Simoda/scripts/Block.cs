c0c30936a2b5a2e027b34be6319d8be16b432b45
Syuvi <durchlaufregal@gmail.com>
2016-10-28 16:08:55 +0900

    押せるブロックの実装、押しているときのプレイヤーの移動実装

---
+++ GRAVYRINTH/Assets/Simoda/scripts/Block.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+    private Transform player;
+    private Transform tr;
+    private Vector3 offset;
+    private Vector3 moveDirection;
+    private Vector3 moveVec;
+    public bool isPush;
+    public float offsetY;
+    public float pushDistance;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        tr = gameObject.transform;
+        offset = PlayerDirectionOffsetY(offsetY);
+        isPush = false;
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.T))
+        //{
+        //    moveDirection = Vector3.Normalize(GetPlayerDirection().normal);
+        //    isPush = true;
+        //    player.GetComponent<PlayerBlockPush>().SetCollisionBlock(gameObject);
+        //}
+
+        //if (Input.GetKeyUp(KeyCode.T))
+        //{
+        //    isPush = false;
+        //    player.GetComponent<PlayerBlockPush>().SetCollisionBlock(null);
+        //}
+
+        print(moveDirection);
+
+        BlockMove();
+    }
+
+    public void BlockMove()
+    {
+        if (isPush == false) return;
+
+        if (Vector3.Dot(player.up, GetPlayerDirection().normal) != 0) return;
+
+        tr.position += moveVec * Time.deltaTime;
+    }
+
+    public RaycastHit GetPlayerDirection()
+    {
+        RaycastHit hitInto;
+        Ray ray = new Ray(player.position + offset, tr.position - (player.position + offset));
+        Physics.Raycast(ray, out hitInto);
+
+        Debug.DrawRay(tr.position, hitInto.normal, Color.red);
+        return hitInto;
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            moveDirection = Vector3.Normalize(GetPlayerDirection().normal);
+            isPush = true;
+            player.GetComponent<PlayerBlockPush>().SetCollisionBlock(gameObject);
+        }
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            isPush = false;
+            player.GetComponent<PlayerBlockPush>().SetCollisionBlock(null);
+        }
+    }
+
+    public void IsPushDistance()
+    {
+        if (Vector3.Distance(tr.position, player.position + offset) > pushDistance) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            moveDirection = Vector3.Normalize(GetPlayerDirection().normal);
+            isPush = true;
+            player.GetComponent<PlayerBlockPush>().SetCollisionBlock(gameObject);
+        }
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            isPush = false;
+            player.GetComponent<PlayerBlockPush>().SetCollisionBlock(null);
+        }
+
+        Debug.DrawRay(tr.position, Vector3.Normalize
+            (((player.position + offset) - tr.position)) * pushDistance, Color.blue);
+    }
+
+    public Vector3 GetBlockMoveDirection()
+    {
+        return moveDirection;
+    }
+
+    public bool GetIsPush()
+    {
+        return isPush;
+    }
+
+    public void SetMoveVector(Vector3 vector)
+    {
+        moveVec = vector;
+    }
+
+    private Vector3 PlayerDirectionOffsetY(float offset)
+    {
+        return new Vector3(0.0f, offset, 0.0f);
+    }
+}
---
+++ GRAVYRINTH/Assets/Simoda/scripts/PlayerBlockPush.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBlockPush : MonoBehaviour
+{
+    //当たったかどうか付のRaycastHit
+    struct RayHitInfo
+    {
+        public RaycastHit hit;
+        //当たったか？
+        public bool isHit;
+    };
+
+    [SerializeField, TooltipAttribute("移動速度")]
+    private float m_MoveSpeed = 3.0f;
+    [SerializeField, TooltipAttribute("身長(地面との判定を行うRayで使用)")]
+    private float m_Height = 2.0f;
+    [SerializeField, TooltipAttribute("斜面と認識する角度（壁と斜面の境界値）")]
+    private float m_SlopeDeg = 45.0f;
+
+    private Transform tr;
+    public Vector3 m_MoveVec;
+    private GravityDirection m_GravityDir;
+    private Block collisionBlock;
+    private Transform m_Camera;
+
+    void Start()
+    {
+        m_GravityDir = GameObject.Find("GravityDirection").GetComponent<GravityDirection>();
+
+        tr = GetComponent<Transform>();
+        m_Camera = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        BlockPushMove();
+
+        //重力の方向を設定
+        m_GravityDir.SetDirection(GetDown());
+    }
+
+    /// <summary>
+    /// 移動方向入力の取得
+    /// </summary>
+    private Vector2 GetMoveInputAxis()
+    {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        return direction;
+    }
+
+    /// <summary>
+    /// 移動方向入力を補正して返す
+    /// </summary>
+    private Vector2 MoveInputCorrection(Vector2 input)
+    {
+        Vector2 direction = input;
+        //加速と減速をいい感じに補正
+        if (direction != Vector2.zero)
+        {
+            float length = direction.magnitude;
+            length = Mathf.Min(1, length);
+            length = length * length;
+            direction = direction.normalized * length;
+        }
+        return direction;
+    }
+
+    /// <summary>
+    /// 現在の下方向を取得
+    /// </summary>
+    private Vector3 GetDown()
+    {
+        return Vector3.Normalize(-tr.up);
+    }
+
+    /// <summary>
+    /// 現在向いている下方向にレイを飛ばし、ヒットした情報を返す。
+    /// </summary>
+    /// <param name="reyPos">レイを飛ばす位置</param>
+    /// <returns></returns>
+    private RayHitInfo CheckGroundHit(Vector3 reyPos)
+    {
+        RayHitInfo result;
+        Ray ray = new Ray(reyPos, GetDown());
+        RaycastHit hit;
+        result.isHit = Physics.Raycast(ray, out hit, m_Height);
+        result.hit = hit;
+
+        //レイをデバッグ表示
+        Debug.DrawRay(reyPos, GetDown() * m_Height, Color.grey, 1.0f, false);
+
+        return result;
+    }
+
+    private void BlockPushMove()
+    {
+        tr.GetComponent<PlayerMove>().enabled = true;
+
+        if (collisionBlock == null) return;
+        collisionBlock.IsPushDistance();
+        if (collisionBlock == null) return;
+
+        if (collisionBlock.isPush == false) return;
+
+        tr.GetComponent<PlayerMove>().enabled = false;
+
+        //移動方向入力
+        Vector2 inputVec = GetMoveInputAxis();
+        //入力された値を移動用に補正
+        Vector2 moveVec = MoveInputCorrection(inputVec);
+
+        Vector3 moveDirection = collisionBlock.GetBlockMoveDirection();
+        m_MoveVec = (moveDirection * -moveVec.y + moveDirection * 0.0f) * m_MoveSpeed;
+
+        collisionBlock.SetMoveVector(m_MoveVec);
+
+        //移動
+        tr.position += m_MoveVec * Time.deltaTime;
+
+        //仮重力
+        tr.position += GetDown() * 1.0f * Time.deltaTime;
+        //地面との判定
+        RayHitInfo hitInfo = CheckGroundHit(tr.position + tr.up * m_Height);
+        if (hitInfo.isHit)
+        {
+            //上方向と平面の法線方向のなす角
+            float angle = Vector3.Angle(tr.up, hitInfo.hit.normal);
+            //斜面として認識する角度以上なら何もしない
+            if (angle > m_SlopeDeg) return;
+
+            //当たった地点に移動
+            tr.position = hitInfo.hit.point;
+            //上方向を当たった平面の法線方向に変更
+            tr.up = hitInfo.hit.normal;
+        }
+    }
+
+    public void SetCollisionBlock(GameObject obj)
+    {
+        try
+        {
+            collisionBlock = obj.GetComponent<Block>();
+        }
+        catch
+        {
+            collisionBlock = null;
+        }
+    }
+
+}
