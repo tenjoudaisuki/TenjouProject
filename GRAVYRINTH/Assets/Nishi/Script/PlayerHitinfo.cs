aab9b49bf9fd1f08c56e8f8d8965ea67751a3b45
Jackal0024 <icemaunntenn@gmail.com>
2016-11-13 18:38:31 +0900

    プレイヤーのリスポーン処理を半追加

---
+++ GRAVYRINTH/Assets/Nishi/Script/PlayerHitinfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHitinfo : MonoBehaviour
+{
+    public Vector3 m_CheckPoint;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "CheckPoint")
+        {
+            m_CheckPoint = other.gameObject.transform.position;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Inside")
+        {
+            transform.parent.transform.position = m_CheckPoint;
+        }
+    }
+}
