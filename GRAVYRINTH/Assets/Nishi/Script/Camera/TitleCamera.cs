491dddd19eb7849de9ce184f65bf317a4eb8f296
Jackal0024 <icemaunntenn@gmail.com>
2016-11-25 14:06:27 +0900

    カメラの機能追加

---
+++ GRAVYRINTH/Assets/Nishi/Script/Camera/TitleCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleCamera : MonoBehaviour {
+
+    public GameObject mStageCenter;
+    public float mSpeed;
+    public float mDistance = 5.0f;
+    public Vector3 mOffset;
+
+    bool isStart = false;
+    Vector3 mFromPos;
+    float mTimer = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isStart = false;
+        transform.position = (mStageCenter.transform.forward * -mDistance) + mOffset;
+        transform.LookAt(mStageCenter.transform);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+            transform.RotateAround(mStageCenter.transform.position, Vector3.up, mSpeed);
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                GetComponent<CameraManager>().StateChange(State.Select);
+            }
+
+        //if(isStart)
+        //{
+        //    if (mTimer >= 1.2f)
+        //    {
+        //        GetComponent<CameraControl>().enabled = true;
+        //        this.enabled = false;
+        //    }
+        //    mTimer += Time.deltaTime;
+        //    GameObject player = GameObject.Find("Player");
+        //    transform.LookAt(player.transform);
+        //    Vector3 nextPos = player.transform.position + (player.transform.forward * -2) + new Vector3(0,0.4f,0);
+        //    Debug.Log(nextPos);
+        //    transform.position = Vector3.Lerp(mFromPos,nextPos,mTimer);
+        //}
+	}
+}
