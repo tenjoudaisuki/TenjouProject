01cca7ea52649cf35700c76333ec88959fe6cf63
unknown <>
2016-11-25 16:39:49 +0900

    生まれ変わったバックログを喰らえ！！ リスポーン処理完成（CheckPointプレハブを任意の場所に配置してください） 水面の表現のために水面シェーダーとプレハブをインポート

---
+++ GRAVYRINTH/Assets/Moriya/Script/CheckPoint.cs
@@ -0,0 +1,30 @@
+﻿/**==========================================================================*/
+/**
+ * プレイヤーのリスポーン地点であるチェックポイントの処理
+ * 作成者：守屋   作成日：16/11/25
+/**==========================================================================*/
+using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour 
+{
+
+    private PlayerRespawn m_PlayerRespawn;
+
+    void Start()
+    {
+        m_PlayerRespawn = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerRespawn>();
+    }
+
+
+    void OnTriggerEnter(Collider coll)
+    {
+        //プレイヤーが当たってきた瞬間
+        if (coll.gameObject.tag == "Player")
+        {
+            //状態を保存させる
+            m_PlayerRespawn.SaveCurrentStatus();
+        }
+    }
+
+}
