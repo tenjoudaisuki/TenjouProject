a54b0504e8ce2409d743131474751bc8eda5e2c9
Jackal0024 <icemaunntenn@gmail.com>
2016-12-02 16:35:49 +0900

    スピンオブジェ追加

---
+++ GRAVYRINTH/Assets/Nishi/Script/SpinObject/SpinChild.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinChild : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float speed = transform.parent.GetComponent<SpinParent>().mSpinSpeed;
+        transform.Rotate(Vector3.forward,speed * 2);
+	}
+}
---
+++ GRAVYRINTH/Assets/Nishi/Script/SpinObject/SpinParent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinParent : MonoBehaviour {
+
+    public float mSpinSpeed;
+    	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.Rotate(transform.up, mSpinSpeed);
+	}
+}
