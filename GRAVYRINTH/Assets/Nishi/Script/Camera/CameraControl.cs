1e51e282f50b19098e152c2815223b86f0e062f2
Jackal0024 <icemaunntenn@gmail.com>
2016-12-09 16:34:11 +0900

    カメラ修正

--- GRAVYRINTH/Assets/Nishi/Script/Camera/CameraControl.cs
+++ GRAVYRINTH/Assets/Nishi/Script/Camera/CameraControl.cs
@@ -20,6 +20,8 @@ public class CameraControl : ICamera
     public Vector3 TargetOffset;
     [SerializeField, TooltipAttribute("回転のスピード")]
     public Vector2 m_RotateSpeed = new Vector2(2.0f,2.0f);
+    [SerializeField, TooltipAttribute("ぶら下り時のカメラ回転のスピード")]
+    public Vector2 m_DangleRotate;
 
     /// <summary>
     /// offsetをモデル座標化
@@ -98,7 +100,7 @@ public class CameraControl : ICamera
     private void TargetAroundMove(Vector3 up,Vector3 right)
     {
         float horizontal = -Input.GetAxisRaw("Horizontal2") * m_RotateSpeed.x * Time.deltaTime;
-        float vertical = -Input.GetAxisRaw("Vertical2") * m_RotateSpeed.y * Time.deltaTime;
+        float vertical = -Input.GetAxis("Vertical2") * m_RotateSpeed.y * Time.deltaTime;
 
         XAxisTotal += vertical;
         //X軸の回転の限界を設定
@@ -111,11 +113,13 @@ public class CameraControl : ICamera
 
         //ターゲットの上ベクトルと自身の横ベクトルの外積で地面と平行なベクトルを作る
         Vector3 parallel = Vector3.Cross(up, right);
-        mParallel = Vector3.Lerp(mParallel, parallel, 0.3f);
+        //mParallel =  Vector3.Lerp(mParallel,parallel, 0.3f);
+        mParallel = parallel;
         //平行ベクトルをターゲットの上ベクトルを軸に回転さらに自身の横ベクトルを軸に回転しカメラの位置を計算
-        Vector3 temp = Quaternion.AngleAxis(XAxisTotal, right) * Quaternion.AngleAxis(horizontal, up) * mParallel;
-        CameraPosDirection = Vector3.Lerp(CameraPosDirection,temp,0.1f);
+        Vector3 temp = Quaternion.AngleAxis(XAxisTotal, transform.right) * Quaternion.AngleAxis(horizontal, up) * mParallel;
 
+        //CameraPosDirection = Vector3.Lerp(CameraPosDirection,temp,0.1f);
+        CameraPosDirection = temp;
         //カメラを移動させる
         CameraMove();
     }
@@ -172,10 +176,10 @@ public class CameraControl : ICamera
         TargetAroundMove(Target.up, transform.right);
 
         //カメラを回転させる
-        transform.localRotation = Quaternion.Slerp(transform.localRotation,
-          Quaternion.LookRotation((Target.position + offset) - transform.position,Target.up), 0.5f);
+        //transform.localRotation = Quaternion.Slerp(transform.localRotation,
+        //  Quaternion.LookRotation((Target.position + offset) - transform.position,Target.up), 0.5f);
         //補間なし版
-        //transform.localRotation = Quaternion.LookRotation((Target.position + offset) - transform.position, Target.up);
+        transform.localRotation = Quaternion.LookRotation((Target.position + offset) - transform.position, Target.up);
 
         if (Target.GetComponent<PlayerMoveManager>().GetState() == PlayerState.IRON_BAR_DANGLE) mCurrentState = State.BraDown;
     }
@@ -200,10 +204,8 @@ public class CameraControl : ICamera
 
     private void BraDown()
     {
-        GameObject player = GameObject.Find("Player");
-
-        float horizontal = -Input.GetAxisRaw("Horizontal2") * 2 * Time.deltaTime;
-        float vertical = -Input.GetAxisRaw("Vertical2") * 2 * Time.deltaTime;
+        float horizontal = -Input.GetAxisRaw("Horizontal2") * m_DangleRotate.x * Time.deltaTime;
+        float vertical = -Input.GetAxisRaw("Vertical2") * m_DangleRotate.y * Time.deltaTime;
 
         XAxisTotal += vertical;
         //X軸の回転の限界を設定
@@ -213,16 +215,16 @@ public class CameraControl : ICamera
         //X軸の回転の限界を設定
         //YAxisTotal = Mathf.Clamp(YAxisTotal, -89, 89);
 
-        Vector3 up = Quaternion.AngleAxis(XAxisTotal, transform.right) * player.transform.up;
+        Vector3 up = Quaternion.AngleAxis(XAxisTotal, transform.right) * Target.transform.up;
         if (Input.GetAxisRaw("Vertical") != 0)
         {
             XAxisTotal = 90;
-            CameraPosDirection = player.transform.up;
-            up = player.transform.forward;
+            CameraPosDirection = Target.transform.up;
+            up = Target.transform.forward;
         }
 
         //ターゲットの上ベクトルと自身の横ベクトルの外積で地面と平行なベクトルを作る
-        Vector3 parallel = Vector3.Cross(player.transform.up, transform.right);
+        Vector3 parallel = Vector3.Cross(Target.transform.up, transform.right);
         //Vector3 parallel = transform.forward;
         mParallel = parallel;
 
@@ -233,7 +235,7 @@ public class CameraControl : ICamera
         }
         else
         {
-            temp = Quaternion.AngleAxis(horizontal, player.transform.up) * Quaternion.AngleAxis(XAxisTotal, transform.right) * mParallel;
+            temp = Quaternion.AngleAxis(horizontal, Target.transform.up) * Quaternion.AngleAxis(XAxisTotal, transform.right) * mParallel;
         }
         CameraPosDirection = temp;
 
@@ -243,7 +245,7 @@ public class CameraControl : ICamera
         Ray ray = new Ray(Target.position, CameraPosDirection.normalized);
 
         //RaycastHit hit;
-        //rayの方向の指定距離以内に障害物が無いか？
+        ////rayの方向の指定距離以内に障害物が無いか？
         //if (Physics.Raycast(ray, out hit, Distance))
         //{
         //    //壁に当たった位置をカメラ位置に
@@ -252,8 +254,8 @@ public class CameraControl : ICamera
         //else
         //{
         //    //当たらなかったらray* Disをカメラ位置に
-        //    next = Target.position + offset) + (CameraPosDirection * Distance;
-        //｝
+        //    next = (Target.position) + (CameraPosDirection * Distance);
+        //}
         next = (Target.position) + (CameraPosDirection * Distance);
         //デバック表示
         Debug.DrawRay(Target.position, CameraPosDirection, Color.yellow);
@@ -266,7 +268,7 @@ public class CameraControl : ICamera
         if (Target.GetComponent<PlayerMoveManager>().GetState() != PlayerState.IRON_BAR_DANGLE) mCurrentState = State.Normal;
         //transform.localRotation = Quaternion.Slerp(transform.localRotation,
         //        Quaternion.LookRotation((player.transform.position) - transform.position,up), 0.5f);
-        transform.localRotation = Quaternion.LookRotation((player.transform.position) - transform.position, up);
+        transform.localRotation = Quaternion.LookRotation((Target.transform.position) - transform.position, up);
 
     }
 
