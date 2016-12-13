ce454e22a1792374fa0925d5b5de4a6db4aab542
unknown <>
2016-12-13 16:46:08 +0900

    ベータまでにつぶすタスクをまとめたtxt追加

--- GRAVYRINTH/Assets/Moriya/Script/WaterMove.cs
+++ GRAVYRINTH/Assets/Moriya/Script/WaterMove.cs
@@ -14,6 +14,8 @@ public class WaterMove : MonoBehaviour
     private float m_SpeedMax = 0.02f;
     [SerializeField, Tooltip("移動速度最小値")]
     private float m_SpeedMin = 0.008f;
+    [SerializeField, Tooltip("原点からの距離")]
+    private float m_Length = 5.01f;
 
     void Awake()
     {
@@ -34,7 +36,7 @@ public class WaterMove : MonoBehaviour
         angle = Mathf.Clamp(angle, m_SpeedMin, m_SpeedMax);
 
         //座標を計算
-        tr.position = Vector3.Lerp(tr.up, player.up, angle) * -5.01f;
+        tr.position = Vector3.Lerp(tr.up, player.up, angle) * - m_Length;
 
         //回転を計算
         tr.rotation = Quaternion.Slerp(tr.rotation, player.rotation, angle);
