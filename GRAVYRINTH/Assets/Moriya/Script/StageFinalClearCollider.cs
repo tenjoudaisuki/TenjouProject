dee0e2f8a758b192e72d946ec3ad614c132eea32
unknown <>
2016-12-16 16:12:51 +0900

    クリアすると（現在はＮキーを押すと）大砲が傾いて、大砲に入れる 大砲に入って飛ぶまでをリリースに統合

--- GRAVYRINTH/Assets/Moriya/Script/StageFinalClearCollider.cs
+++ GRAVYRINTH/Assets/Moriya/Script/StageFinalClearCollider.cs
@@ -3,6 +3,27 @@ using System.Collections;
 
 public class StageFinalClearCollider : MonoBehaviour 
 {
+    private GameObject m_Taihou;
+
+    void Start()
+    {
+        m_Taihou = GameObject.FindGameObjectWithTag("Taihou");
+        GetComponent<SphereCollider>().enabled = false;
+    }
+
+    void Update()
+    {
+        //クリアしたら
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            //当たり判定を有効にする
+            GetComponent<SphereCollider>().enabled = true;
+            //大砲を傾ける
+            m_Taihou.transform.rotation = Quaternion.Euler( new Vector3(-12, 0, 0));
+        }
+    }
+
+
     void OnTriggerEnter(Collider coll)
     {
         if(coll.tag == "Player")
