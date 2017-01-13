42402a65d04abb29955d93a1128681ad54f5d5bd
Jackal0024 <icemaunntenn@gmail.com>
2017-01-13 10:12:17 +0900

    ステージf2を更新

---
+++ GRAVYRINTH/Assets/Nishi/Script/CannonUVScroll.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonUVScroll : MonoBehaviour {
+
+    [SerializeField]
+    private float scrollSpeedX = 0.1f;
+
+    [SerializeField]
+    private float scrollSpeedY = 0.1f;
+
+    float rad;
+
+    void Start()
+    {
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", Vector2.zero);
+    }
+
+    void Update()
+    {
+        rad += Time.deltaTime;
+
+        var x = Mathf.Repeat(Time.time * scrollSpeedX, 1);
+        var y = Mathf.Repeat(Time.time * scrollSpeedY, 1);
+
+        var offset = new Vector2(x, y);
+
+        if (transform.GetComponent<SkinnedMeshRenderer>())
+        {
+            var skr = transform.GetComponent<SkinnedMeshRenderer>();
+            var materials = skr.materials;
+            materials[1].SetTextureOffset("_MainTex", offset);
+        }
+        else
+        {
+            var skr = transform.GetComponent<MeshRenderer>();
+            var materials = skr.materials;
+            materials[0].SetTextureOffset("_MainTex", offset);
+        }
+    }
+}
