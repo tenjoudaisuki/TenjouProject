4d5b41556fc4fe43d68b5772ca1c8e337d998fd1
Jackal0024 <icemaunntenn@gmail.com>
2017-01-14 11:36:41 +0900

    紅い壁の処理追加

---
+++ GRAVYRINTH/Assets/Nishi/Script/RedWallEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedWallEffect : MonoBehaviour
+{
+
+    GameObject mCamera;
+    public float mMax;
+    public float mMin;
+
+    // Use this for initialization
+    void Start()
+    {
+        mCamera = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPos = mCamera.transform.position;
+
+        float distance = Vector3.Distance(transform.position, cameraPos);
+        distance = Mathf.Clamp(distance, mMin,mMax);
+        float f = ((distance - mMin) / (mMax - mMin));
+
+        var skr = transform.GetComponent<MeshRenderer>();
+        var material = skr.material;
+
+        Color color = material.GetColor("_TintColor");
+        color.a = Mathf.Lerp(0.5f, 0.1f, f);
+        material.SetColor("_TintColor", color);
+
+        var chaildRenderer = gameObject.GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer renderer in chaildRenderer)
+        {
+            renderer.material.SetColor("_TintColor", color);
+        }
+
+    }
+}
