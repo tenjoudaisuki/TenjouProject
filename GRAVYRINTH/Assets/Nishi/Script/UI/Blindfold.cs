9f6a61528a93d54977e5c0569fbce1b7a8dbaf20
Jackal0024 <icemaunntenn@gmail.com>
2017-01-25 22:33:11 +0900

    ステージf用ブラインドの用意

---
+++ GRAVYRINTH/Assets/Nishi/Script/UI/Blindfold.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Blindfold : MonoBehaviour {
+
+    public float mFadeInStartTime;
+    public float mFadeInTime;
+    public float mFadeOutStartTime;
+    public float mFadeOutTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        RectTransform image = GetComponent<Image>().rectTransform;
+        LeanTween.alpha(image, 1, mFadeInTime).setDelay(mFadeInStartTime)
+        .setOnComplete(() => { LeanTween.alpha(image, 0, mFadeOutTime).setDelay(mFadeOutStartTime).setOnComplete(() => { Destroy(gameObject); }); });
+	}
+}
