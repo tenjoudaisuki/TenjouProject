303bf577ac790be898db0763f0a45bfe66675a58
Syuvi <durchlaufregal@gmail.com>
2017-01-26 19:26:35 +0900

    メニューを変更　ポーズ中のメニューはまだ

--- GRAVYRINTH/Assets/Simoda/scripts/UI/UIInputManager.cs
+++ GRAVYRINTH/Assets/Simoda/scripts/UI/UIInputManager.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class UIInputManager : MonoBehaviour
 {
+    private System.Action submitAction;
+
+    private System.Action cancelAction;
+
     void Start()
     {
 
@@ -41,4 +45,27 @@ public class UIInputManager : MonoBehaviour
             action();
     }
 
+    //決定入力時の処理を設定
+    public void SetSubmitAction(System.Action action)
+    {
+        submitAction = action;
+    }
+
+    //決定入力時、処理
+    public void Submit()
+    {
+        submitAction();
+    }
+
+    //キャンセル入力時の処理を設定
+    public void SetCancelAction(System.Action action)
+    {
+        cancelAction = action;
+    }
+
+    //キャンセル入力時、処理
+    public void Cancel()
+    {
+        cancelAction();
+    }
 }
