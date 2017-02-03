000241dfa6c01845ac2bde7f282209f89f8933b6
Jackal0024 <icemaunntenn@gmail.com>
2017-02-03 12:13:51 +0900

    イベントカメラ　修正

--- GRAVYRINTH/Assets/Nishi/Script/StartEvent.cs
+++ GRAVYRINTH/Assets/Nishi/Script/StartEvent.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartEvent : MonoBehaviour {
+public class StartEvent : MonoBehaviour
+{
 
+    public EventPoint mNextPoint;
     public GameObject mCameraPoint;
     /// <summary>
     /// 表示するテキスト
     /// </summary>
     public GameObject DrawUI;
-    // Use this for initialization
-    void Start () {
-        GameObject.Find("Camera").GetComponent<EventCamera>().SetMoveTime(1.0f);
-        GameObject.Find("Camera").GetComponent<EventCamera>().SetBotton(true);
-        GameObject.Find("Camera").GetComponent<EventCamera>().SetTarget(mCameraPoint);
+    private bool isFirst;
 
-        GameObject.Find("Camera").GetComponent<CameraManager>().StateChange(State.Event);
-        Instantiate(DrawUI);
+    public void Start()
+    {
+        isFirst = false;
+    }
+
+    public void Update()
+    {
+        if (GameManager.Instance.GetMode() == GameManager.GameMode.GamePlay && !isFirst)
+        {
+            GameObject.Find("Camera").GetComponent<EventCamera>().SetCompleteAction(
+                () => { mNextPoint.isActive = true; }
+                );
+            GameObject.Find("Camera").GetComponent<EventCamera>().SetMoveTime(1.0f);
+            GameObject.Find("Camera").GetComponent<EventCamera>().SetBotton(true);
+            GameObject.Find("Camera").GetComponent<EventCamera>().SetTarget(mCameraPoint);
+
+            GameObject.Find("Camera").GetComponent<CameraManager>().StateChange(State.Event);
+            Instantiate(DrawUI);
+            isFirst = true;
+        }
 
     }
 }
