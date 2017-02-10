1bec70883ca9ef1621f09787e17b2a06e4fd6ee0
Jackal0024 <icemaunntenn@gmail.com>
2017-02-10 10:23:35 +0900

    スイッチマネージャーに音を追加予定　SwitchManager.csをみて

--- GRAVYRINTH/Assets/Nishi/Script/SwitchScript/SwitchManager.cs
+++ GRAVYRINTH/Assets/Nishi/Script/SwitchScript/SwitchManager.cs
@@ -37,13 +37,16 @@ public class SwitchManager : MonoBehaviour
     {
         if(isActive && other.tag == "Player")
         {
+            //ゴゴゴと扉が開く音
+            //SoundManager.Instance.PlaySe("");
             GameObject.Find("Camera").GetComponent<EventCamera>().SetMoveTime(2.0f);
             GameObject.Find("Camera").GetComponent<EventCamera>().SetEventEndTime(3.0f);
             GameObject.Find("Camera").GetComponent<EventCamera>().SetTarget(mCameraPos);
 
             GameObject.Find("Camera").GetComponent<CameraManager>().StateChange(State.Event);
 
-            LeanTween.rotate(mFinalDoor, new Vector3(90, 0, 0), mOpenTime);
+            //扉が開いたときの音
+            LeanTween.rotate(mFinalDoor, new Vector3(90, 0, 0), mOpenTime).setOnComplete(()=> { SoundManager.Instance.PlaySe("doom"); });
             gameObject.SetActive(false);
 
             GameObject.Find("BGMControl").GetComponent<BGMControl>().PlayerFinalDoorSwitchTouched();
